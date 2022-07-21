59e0063e25583dfb1311de02903bdfd6d6536947
ayomideajewole <ayomiwole1@gmail.com>
2022-07-21 01:33:47 +0100

    Updated environment variables

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME="postgres"
 export POSTGRES_PASSWORD="postgres"
-export POSTGRES_HOST="udagram-ayomide-dev.csoyt0z90wnu.us-east-1.rds.amazonaws.com"
+export POSTGRES_HOST="udagrampostgres.c4sfihcv4og1.us-east-1.rds.amazonaws.com"
 export POSTGRES_DB="postgres"
-export AWS_BUCKET="udagram-ayomide-dev"
+export AWS_BUCKET="udagrambucket1"
 export AWS_REGION="us-east-1"
-export AWS_PROFILE="default"
+export AWS_PROFILE="Wunderkind"
 export JWT_SECRET="testing"
 export URL=http://localhost:8100
